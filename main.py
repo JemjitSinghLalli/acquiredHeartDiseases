4a3cf8daa4b47705f864c312c57e7e716938ac3a
LalliJ <jemjit.lalli@bca.com>
2022-12-20 17:05:14 +0000

    created a correlation matrix based on mutual information, created a graph using maximum spanning tree and plotted using nx

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """
 This is the basic runner for structuring graphs and performing ML over graphs
 """
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.algorithms import tree
+from sklearn.metrics import mutual_info_score
 
 from utils.load.data_importing import import_csv_data
 from utils.preprocessing.generic_preprocessing import (
@@ -16,7 +20,11 @@ df = convert_columns_to_correct_types(df)
 
 numeric_df = reduce_data_frame_to_numeric_columns(df)
 numeric_df = bin_numeric_data(numeric_df, 5)
-
 categorical_df = reduce_data_frame_to_categorical_columns(df, list(numeric_df.columns))
-
 training_df = numeric_df.join(categorical_df)
+correlation_matrix = training_df.corr(method=mutual_info_score)
+del categorical_df
+del numeric_df
+graph = tree.maximum_spanning_tree(nx.from_pandas_adjacency(correlation_matrix))
+nx.draw(graph, with_labels=True)
+plt.show()
